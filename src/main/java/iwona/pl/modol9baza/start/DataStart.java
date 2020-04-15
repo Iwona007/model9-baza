c7c12514718b3adcd3dbe0e75c3ef7fada099508
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-04-15 15:21:18 +0200

    new measurement

--- src/main/java/iwona/pl/modol9baza/start/DataStart.java
+++ src/main/java/iwona/pl/modol9baza/start/DataStart.java
@@ -19,9 +19,8 @@ public class DataStart {
 
     @EventListener(ApplicationReadyEvent.class)
     public void read() {
-        dataService.read(); //h2 0.014, 0.008 // mysql 0.007, 0.008, 0.108, 0.006
-
-        dataService.saveAll(dataService.getDataList()); // H2 0.287, 0.269 // mysql 72.55, 72.329 , 74.786, 77.852
-        dataService.find(); //H2 0.13, 0.118 // mysql 0.73, 0.792, 0.931, 1072
+        dataService.read(); //h2 0.014, 0.008, 0.006, 0.014 // mysql 0.007, 0.008, 0.108, 0.006
+        dataService.saveAll(dataService.getDataList()); // H2 0.287, 0.269, 0.281, 0.397 // mysql 72.55, 72.329 , 74.786, 77.852
+        dataService.find(); //H2 0.13, 0.118, 0.125, 0.125 // mysql 0.73, 0.792, 0.931, 1072
     }
 }
